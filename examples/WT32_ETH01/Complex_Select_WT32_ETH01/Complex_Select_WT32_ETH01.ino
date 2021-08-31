c5e36db87f7290b80fb69f463e6f81012a63484b
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-08-30 22:13:17 -0400

    v1.3.0 to support NativeEthernet
    ### Release v1.3.0
    
    1. Add support to **Teensy 4.1 using NativeEthernet**

--- examples/WT32_ETH01/Complex_Select_WT32_ETH01/Complex_Select_WT32_ETH01.ino
+++ examples/WT32_ETH01/Complex_Select_WT32_ETH01/Complex_Select_WT32_ETH01.ino
@@ -11,17 +11,6 @@
 
   Built by Khoi Hoang https://github.com/khoih-prog/MySQL_MariaDB_Generic
   Licensed under MIT license
-  Version: 1.2.0
-
-  Version Modified By   Date      Comments
-  ------- -----------  ---------- -----------
-  1.0.0   K Hoang      13/08/2020 Initial coding/porting to support nRF52, SAM DUE and SAMD21/SAMD51 boards using W5x00 Ethernet
-                                  (Ethernet, EthernetLarge, Ethernet2, Ethernet3 library), WiFiNINA and ESP8266/ESP32-AT shields
-  1.0.1   K Hoang      18/08/2020 Add support to Ethernet ENC28J60. Fix bug, optimize code.
-  1.0.2   K Hoang      20/08/2020 Fix crashing bug when timeout. Make code more error-proof. Drop support to ESP8266_AT_Webserver.
-  1.0.3   K Hoang      02/10/2020 Add support to Ethernet ENC28J60 using new EthernetENC library.
-  1.1.0   K Hoang      08/06/2021 Add support to RP2040-based boards such as Nano_RP2040_Connect, RASPBERRY_PI_PICO. etc.
-  1.2.0   K Hoang      20/07/2021 Add support to WT32_ETH01 (ESP32 + LAN8720A)
  **********************************************************************************************************************************/
 /*
   MySQL Connector/Arduino Example : complex select
@@ -65,7 +54,7 @@
 
 #include <WebServer_WT32_ETH01.h>
 
-#include <MySQL_Generic_WiFi.h>
+#include <MySQL_Generic.h>
 
 // Select the IP address according to your local network
 IPAddress myIP(192, 168, 2, 232);
--- examples/WiFiNINA/Complex_Insert_WiFiNINA/Complex_Insert_WiFiNINA.ino
+++ examples/WiFiNINA/Complex_Insert_WiFiNINA/Complex_Insert_WiFiNINA.ino
@@ -11,17 +11,6 @@
 
   Built by Khoi Hoang https://github.com/khoih-prog/MySQL_MariaDB_Generic
   Licensed under MIT license
-  Version: 1.2.0
-
-  Version Modified By   Date      Comments
-  ------- -----------  ---------- -----------
-  1.0.0   K Hoang      13/08/2020 Initial coding/porting to support nRF52, SAM DUE and SAMD21/SAMD51 boards using W5x00 Ethernet
-                                  (Ethernet, EthernetLarge, Ethernet2, Ethernet3 library), WiFiNINA and ESP8266/ESP32-AT shields
-  1.0.1   K Hoang      18/08/2020 Add support to Ethernet ENC28J60. Fix bug, optimize code.
-  1.0.2   K Hoang      20/08/2020 Fix crashing bug when timeout. Make code more error-proof. Drop support to ESP8266_AT_Webserver.
-  1.0.3   K Hoang      02/10/2020 Add support to Ethernet ENC28J60 using new EthernetENC library.
-  1.1.0   K Hoang      08/06/2021 Add support to RP2040-based boards such as Nano_RP2040_Connect, RASPBERRY_PI_PICO. etc.
-  1.2.0   K Hoang      20/07/2021 Add support to WT32_ETH01 (ESP32 + LAN8720A)
  **********************************************************************************************************************************/
 /*
   MySQL Connector/Arduino Example : complex insert
@@ -79,7 +68,7 @@
 #include "defines.h"
 #include "Credentials.h"
 
-#include <MySQL_Generic_WiFiNINA.h>
+#include <MySQL_Generic.h>
 
 IPAddress server_addr(192, 168, 2, 112);
 uint16_t server_port = 5698;    //3306;
