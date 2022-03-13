f610ec44e867f599c1d1ee096a8ddcb98160849b
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-03-12 21:42:29 -0500

    v1.7.0 to convert to `h-only` style
    ### Releases v1.7.0
    
    1. Convert to `h-only` style
    2. Add `multiFileProject` examples to demo for multiple-file projects

---
+++ examples/Ethernet/multiFileProject_Ethernet/multiFileProject_Ethernet.ino
@@ -0,0 +1,47 @@
+/****************************************************************************************************************************
+  multiFileProject.ino
+  Library for communicating with a MySQL or MariaDB Server
+  
+  Based on and modified from Dr. Charles A. Bell's MySQL_Connector_Arduino Library https://github.com/ChuckBell/MySQL_Connector_Arduino
+  to support nRF52, SAMD21/SAMD51, SAM DUE, STM32F/L/H/G/WB/MP1, ESP8266, ESP32, etc. boards using W5x00, ENC28J60, LAM8742A Ethernet,
+  WiFiNINA, ESP-AT, built-in ESP8266/ESP32 WiFi.
+
+  The library provides simple and easy Client interface to MySQL or MariaDB Server.
+  
+  Built by Khoi Hoang https://github.com/khoih-prog/MySQL_MariaDB_Generic
+  Licensed under MIT license
+*****************************************************************************************************************************/
+
+// To demo how to include files in multi-file Projects
+
+#include "defines.h"
+
+#define MYSQL_MARIADB_GENERIC_VERSION_MIN_TARGET      "MySQL_MariaDB_Generic v1.7.0"
+#define MYSQL_MARIADB_GENERIC_VERSION_MIN             1007000
+
+#include "multiFileProject.h"
+
+// To be included only in main(), .ino with setup() to avoid `Multiple Definitions` Linker Error
+#include "MySQL_Generic.h"
+
+void setup() 
+{
+  Serial.begin(115200);
+  while (!Serial);
+  
+  Serial.println("\nStart multiFileProject");
+  Serial.println(MYSQL_MARIADB_GENERIC_VERSION);
+
+#if defined(MYSQL_MARIADB_GENERIC_VERSION_MIN)
+  if (MYSQL_MARIADB_GENERIC_VERSION_INT < MYSQL_MARIADB_GENERIC_VERSION_MIN)
+  {
+    Serial.print("Warning. Must use this example on Version equal or later than : ");
+    Serial.println(MYSQL_MARIADB_GENERIC_VERSION_MIN_TARGET);
+  }
+#endif
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
